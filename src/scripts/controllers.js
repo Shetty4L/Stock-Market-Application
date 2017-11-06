f02a587ab4d7acaa2b40307435873751009c3e0e
shetty4l <suyashshetty29@gmail.com>
2017-11-06 15:42:58 -0800

    modified the updateFavorites function to build an array of promises and then modify the favorites array after it has been resolved

--- src/scripts/controllers.js
+++ src/scripts/controllers.js
@@ -10,7 +10,7 @@
             templateUrl: '../views/favorite-list.html',
             controller: 'favoriteController',
             resolve: {
-              getFavorites: function() {
+              getFavorites: function($http, $q) {
                 var favorites = [];
                 for(var fav in localStorage) {
                   if(fav != 'id') {
@@ -54,21 +54,6 @@
       })
       .controller('favoriteController', function($scope, $rootScope, $state, $http, $q, formatResponseService, currentStockData, getFavorites, plotChart, appOpenedForTheFirstTime) {
         $scope.orderKey = '';
-        if(appOpenedForTheFirstTime.get()) {
-          $scope.favorites = getFavorites;
-          appOpenedForTheFirstTime.set(false);
-        } else {
-          var favorites = [];
-          for(var fav in localStorage) {
-            if(fav != 'id') {
-              if(localStorage.hasOwnProperty(fav)) {
-                var item = angular.fromJson(localStorage.getItem(fav));
-                favorites.push(item);
-              }
-            }
-          }
-          $scope.favorites = favorites;
-        }
         $scope.stockKeys = {
             keys: [{
                 value: "id",
@@ -135,65 +120,60 @@
           }
         };
 
-        $scope.updateFavorites = function() {
-          console.log('updating favorites');
-          $scope.manualUpdate = true;
-          var deferred = $q.defer();
+        $scope.updateFavorites = function(manualUpdate) {
+          $scope.manualUpdate = manualUpdate;
+          var promises = [];
           var keys = Object.keys(localStorage);
-          var favorites = [];
           angular.forEach(keys, function(key) {
             var stock = angular.fromJson(localStorage[key]);
             if(stock.symbol) {
-              var config = {
+              var promise = $http.get('/stock', {
                 params: {
                   stockSymbol: stock.symbol,
-                  outputsize:'compact'
+                  outputsize: 'compact'
                 }
-              };
-              $http.get('/stock', config)
-                .then(function(response){
-                  if(response.status == 200) {
-                    var id = stock.id;
-                    var obj = response.data;
-                    obj.last_price = {
-                      value: obj.last_price,
-                      text: obj.last_price.toFixed(2)
-                    };
-                    obj.change = {
-                      value: obj.change,
-                      text: obj.change.toFixed(2)
-                    };
-                    obj.change_percent = {
-                      value: obj.change_percent,
-                      text: obj.change_percent.toFixed(2)
-                    };
-                    obj.volume = {
-                      value: obj.volume,
-                      text: obj.volume.toLocaleString()
-                    };
-                    obj["id"] = id;
-                    localStorage.setItem(obj.symbol, angular.toJson(obj));
-                    favorites.push(angular.fromJson(localStorage[key]));
-                    deferred.resolve(favorites);
-                  }
-
-                })
-                .catch(function(error) {
-                  console.log(error);
-                  favorites.push(angular.fromJson(localStorage[key]));
-                  deferred.resolve(favorites);
-                });
+              })
+              .then(function(response) {
+                if(response.status == 200) {
+                  var id = stock.id;
+                  var obj = response.data;
+                  obj.last_price = {
+                    value: obj.last_price,
+                    text: obj.last_price.toFixed(2)
+                  };
+                  obj.change = {
+                    value: obj.change,
+                    text: obj.change.toFixed(2)
+                  };
+                  obj.change_percent = {
+                    value: obj.change_percent,
+                    text: obj.change_percent.toFixed(2)
+                  };
+                  obj.volume = {
+                    value: obj.volume,
+                    text: obj.volume.toLocaleString()
+                  };
+                  obj["id"] = id;
+                  localStorage.setItem(obj.symbol, angular.toJson(obj));
+                  return obj;
+                } else {
+                  console.log(response);
+                  return angular.fromJson(localStorage.getItem(stock.symbol));
+                }
+              })
+              .catch(function(error) {
+                console.log(error);
+                return angular.fromJson(localStorage.getItem(stock.symbol));
+              });
+              promises.push(promise);
             }
           });
-          $scope.$watch(function() {
-            return favorites.length;
-          }, function() {
-            if($scope.favorites.length == favorites.length) {
-                $scope.favorites = favorites;
-                $scope.manualUpdate = false;
-            }
+          $q.all(promises).then(function(favoritesData) {
+            console.log(favoritesData);
+            $scope.manualUpdate = false;
+            $scope.favorites = favoritesData;
           });
-        };
+        }
 
         $scope.goToStockDetails = function() {
           if($scope.stockData) {
@@ -267,6 +247,23 @@
               console.log(error);
             });
         };
+
+        if(appOpenedForTheFirstTime.get()) {
+          $scope.favorites = getFavorites;
+          appOpenedForTheFirstTime.set(false);
+          $scope.updateFavorites(false);
+        } else {
+          var favorites = [];
+          for(var fav in localStorage) {
+            if(fav != 'id') {
+              if(localStorage.hasOwnProperty(fav)) {
+                var item = angular.fromJson(localStorage.getItem(fav));
+                favorites.push(item);
+              }
+            }
+          }
+          $scope.favorites = favorites;
+        }
       })
       .controller('stockDetailsController', function($scope, $transitions) {
         $scope.selectedState = 'stock.current';
