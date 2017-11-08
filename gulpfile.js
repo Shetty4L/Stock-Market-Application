f2fd1b73ab7b6606b05a1e53f40cb7de9dd70b07
shetty4l <suyashshetty29@gmail.com>
2017-11-08 12:54:47 -0800

    new gulpfile for handling production files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
     cleanCss = require('gulp-clean-css'),
     pump = require('pump'),
     browserSync = require('browser-sync').create(),
@@ -10,7 +11,10 @@ var gulp = require('gulp'),
     es = require('event-stream'),
     mocha = require('gulp-mocha'),
     nodemon = require('gulp-nodemon'),
-    series = require('stream-series');
+    series = require('stream-series'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    exec = require('child_process').exec;
 
 gulp.task('default', ['serve'], function() {
   return gutil.log('Gulp is running properly');
@@ -22,24 +26,50 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('uglify', function(cb) {
+gulp.task('uglify', ['concat-js'], function(cb) {
   pump([
-      gulp.src('src/scripts/**/*.js'),
-      uglify(),
+      gulp.src('dist/scripts/scripts.js'),
+      uglify({mangle:false}),
+      rename({suffix: '.min'}),
       gulp.dest('dist/scripts')
     ], cb);
 });
 
-gulp.task('minify-css', function(cb) {
+gulp.task('minify-css', ['concat-css'], function(cb) {
   pump([
-    gulp.src('src/styles/**/*.css'),
+    gulp.src('dist/styles/styles.css'),
     cleanCss({debug: true}, function(details) {
         console.log(details.name + ': ' + details.stats.originalSize);
         console.log(details.name + ': ' + details.stats.minifiedSize);
       }),
-      gulp.dest('dist/styles')
+    rename({suffix: '.min'}),
+    gulp.dest('dist/styles')
   ], cb);
 });
+
+gulp.task('concat-css', function() {
+  var src = ['src/styles/reset/reset.css'];
+  var regex = /.*\.css/;
+  bowerFiles().forEach(function(filename) {
+    if(regex.test(filename)) src.push(filename);
+  });
+  src.push('src/styles/*.css');
+  return gulp.src(src)
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('concat-js', function() {
+  var src = [];
+  var regex = /.*\.js/;
+  bowerFiles().forEach(function(filename) {
+    if(regex.test(filename)) src.push(filename);
+  });
+  src.push('src/scripts/*.js');
+  return gulp.src(src)
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('dist/scripts'));
+})
 
 gulp.task('watch', ['nodemon'], function() {
   gulp.watch('src/**/*.*', ['jshint', 'inject', browserSync.reload]);
@@ -53,26 +83,34 @@ gulp.task('serve', ['jshint', 'inject', 'watch'], function() {
   });
 });
 
-gulp.task('serve:dist', ['jshint', 'uglify', 'minify-css'], function() {
-  browserSync.init({
-    server: {
-      baseDir: 'dist'
-    }
+gulp.task('serve:dist', function() {
+  console.log('Serving production server');
+  exec('node dist/app.js', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
   });
 });
 
 gulp.task('inject', function() {
-  var reset = gulp.src('src/styles/reset.css', {read: false});
+  var reset = gulp.src('src/styles/reset/reset.css', {read: false});
   var bower = gulp.src(bowerFiles(), {read: false});
   var others = gulp.src([
       './src/scripts/**/*.js',
-      '!src/styles/reset.css',
-      './src/styles/**/*.css'
+      './src/styles/*.css'
     ], {read:false});
 
   gulp.src('./src/views/index.html')
-  .pipe(inject(series(reset, bower, others), {relative:true, ignoreFilePath: 'bower_components'}))
-  .pipe(gulp.dest('./src/views'));
+    .pipe(inject(series(reset, bower, others), {relative:true, ignoreFilePath: 'bower_components'}))
+    .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('inject:dist', ['uglify', 'minify-css'], function() {
+  del(['dist/scripts/scripts.js','dist/styles/styles.css']);
+
+  gulp.src('./dist/views/index.html')
+    .pipe(inject(gulp.src(['dist/styles/*.min.css','dist/scripts/*.min.js']), {relative:true}))
+    .pipe(gulp.dest('./dist/views'));
 });
 
 gulp.task('nodemon', function (cb) {
